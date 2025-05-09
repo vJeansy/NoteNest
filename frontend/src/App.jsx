31dc2688de24488298a0709c6c5f794584f33740
Jeansy Pena <jeansypenar@outlook.com>
2025-05-08 23:13:51 -0400

    updated App,jsx for Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthForm from "./pages/AuthForm";
 import NotesDashboard from "./pages/NotesDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<AuthForm />} />
-      <Route path="/" element={<AuthForm />} />
-      <Route path="/register" element={<AuthForm />} />
-      <Route path="/notes" element={<ProtectedRoute />}>
-        <Route index element={<NotesDashboard />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<AuthForm />} />
+        <Route path="/" element={<AuthForm />} />
+        <Route path="/register" element={<AuthForm />} />
+        <Route path="/notes" element={<ProtectedRoute />}>
+          <Route index element={<NotesDashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
