3fe017eba8ca531a8963ff08f1039f3b9783539b
Jeansy Pena <jeansypenar@outlook.com>
2025-05-08 22:31:37 -0400

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
