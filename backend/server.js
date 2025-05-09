d965cc1a88547839fd4db98ecfffc6594759f42e
Jeansy Pena <jeansypenar@outlook.com>
2025-05-08 23:33:18 -0400

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    "https://notenest-alpha.vercel.app",
-    "https://notenest-orfy3tc5q-vjeansys-projects.vercel.app",
-    "https://notenest-207utcs2m-vjeansys-projects.vercel.app"
+    "https://notenest-alpha.vercel.app", // Production
+    "https://notenest-orfy3tc5q-vjeansys-projects.vercel.app", // Preview 1
+    "https://notenest-207utcs2m-vjeansys-projects.vercel.app"  // Preview 2
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
